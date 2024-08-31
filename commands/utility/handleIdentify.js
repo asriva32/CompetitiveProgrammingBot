6d2b04fc288b97e75e978a864aa23df0bfd3f61d
Arnav Srivastava <arnavsrivastava@Arnavs-MacBook-Pro-5.local>
2024-08-31 12:47:29 -0400

    changed file

--- commands/utility/handleIdentify.js
+++ commands/utility/handleIdentify.js
@@ -2,8 +2,8 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setName('handle-identify')
+		.setDescription('Sets codeforces handle'),
 	async execute(interaction) {
 		await interaction.reply('Pong!');
 	},
