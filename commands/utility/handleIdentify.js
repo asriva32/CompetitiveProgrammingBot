cda943ca8f6e2a9c42b03f64d43029a011c7263f
Arnav Srivastava <arnavsrivastava@Arnavs-MacBook-Pro-5.local>
2024-09-01 17:40:48 -0400

    additions to handleidentify

--- commands/utility/handleIdentify.js
+++ commands/utility/handleIdentify.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 const mongoose = require('mongoose');
 
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('handle-identify')
@@ -12,6 +13,33 @@ module.exports = {
 		),
 	async execute(interaction) {
 		const handle = interaction.options.getString('handle');
-		await interaction.reply('Submit a complile error to');
+		const userID = interaction.user.id;
+		// check if there is already a handle connected to the discord user
+		// check if handle exists in codeforces database
+		// then check if handle is already used by someone else
+		const url = 'https://codeforces.com/api/problemset.problems';
+		let problemUrl = '';
+		try{
+			const response = await fetch(url);
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+			let data = await response.json();
+			if(data.status !== "OK"){
+				await interaction.reply('Codeforces api is down');
+				return;
+			}
+			let Problems = data.result.problems;
+			let len = Problems.length;
+			let index = Math.floor(Math.random() * len);
+			console.log( Problems[index].contestId);
+			problemUrl = 'https://codeforces.com/problemset/problem/' + Problems[index].contestId + '/' + Problems[index].index;
+		} catch (error) {
+			console.error(error.message);
+			return;
+		}
+		await interaction.reply('Submit a complile error to ' + problemUrl + ' within 60 seconds');
+		await new Promise(resolve => setTimeout(resolve, 60000));
+
 	},
 };
