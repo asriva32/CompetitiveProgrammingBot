0ee7f588bbaedd082c4673aeaf0f600a9746980c
Arnav Srivastava <arnavsrivastava@Arnavs-MacBook-Pro-5.local>
2024-08-31 20:07:42 -0400

    added to handleidentify

--- commands/utility/handleIdentify.js
+++ commands/utility/handleIdentify.js
@@ -1,10 +1,17 @@
 const { SlashCommandBuilder } = require('discord.js');
+const mongoose = require('mongoose');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('handle-identify')
-		.setDescription('Sets codeforces handle'),
+		.setDescription('Sets codeforces handle')
+		.addStringOption(option => 
+            option.setName('handle')
+                .setDescription('The user handle to identify')
+                .setRequired(true)
+		),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		const handle = interaction.options.getString('handle');
+		await interaction.reply('Submit a complile error to');
 	},
 };
